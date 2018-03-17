e81db3395f430a293c28fc5022379a437ae0a73b
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2018-03-17 14:30:34 +0900

    Execution on war

--- src/main/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplication.java
+++ src/main/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplication.java
@@ -2,13 +2,15 @@ package jp.fieldnotes.spring.azure.bootonazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class BootOnAzureApplication {
+public class BootOnAzureApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootOnAzureApplication.class, args);
@@ -17,6 +19,11 @@ public class BootOnAzureApplication {
 	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String getSomething() {
 		return "Hello, Azure!";
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BootOnAzureApplication.class);
 	}
 
 }
