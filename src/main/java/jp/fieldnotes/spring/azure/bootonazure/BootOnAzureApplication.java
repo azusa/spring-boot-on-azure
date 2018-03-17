82eee52faacaa122e10e9186de7376d77e70ea87
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2018-03-17 14:04:05 +0900

    Initial Commit

---
+++ src/main/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplication.java
@@ -0,0 +1,12 @@
+package jp.fieldnotes.spring.azure.bootonazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOnAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootOnAzureApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.fieldnotes.spring.azure.bootonazure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootOnAzureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
