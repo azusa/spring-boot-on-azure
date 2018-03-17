3bc724f97c05cb1fe82e49f274c2e3e5698c9a80
Hiroyuki Onaka <hiroyuki@fieldnotes.jp>
2018-03-17 14:14:52 +0900

    Add Controller

--- src/main/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplication.java
+++ src/main/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplication.java
@@ -2,11 +2,21 @@ package jp.fieldnotes.spring.azure.bootonazure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BootOnAzureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootOnAzureApplication.class, args);
 	}
+
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	public String getSomething() {
+		return "Hello, Azure!";
+	}
+
 }
--- src/test/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplicationTests.java
+++ src/test/java/jp/fieldnotes/spring/azure/bootonazure/BootOnAzureApplicationTests.java
@@ -2,15 +2,33 @@ package jp.fieldnotes.spring.azure.bootonazure;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(BootOnAzureApplication.class)
 public class BootOnAzureApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void helloTest() throws Exception {
+		mockMvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, Azure!")));
 	}
 
 }
